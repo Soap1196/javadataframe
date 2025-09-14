3cce311630f4f2bb8e56e15193b52c76094242a8
Jacob Mckinstry <Jdmack007@aol.com>
2025-09-14 10:05:40 -0500

    added dataframe and fixed gitignore

--- src/main/java/com/javadataframe/Main.java
+++ src/main/java/com/javadataframe/Main.java
@@ -1,15 +1,28 @@
 package com.javadataframe;
 
 import org.apache.spark.sql.DataFrameReader;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 
 public class Main {
     public static void main(String[] args) {
         SparkSession session = SparkSession.builder()
-            .appName("TouristDataFrameExample")
-            .master("local[*]")
-            .getOrCreate();
+                .appName("TouristDataFrameExample")
+                .master("local[2]")
+                .getOrCreate();
 
-        DataFrameReader dataFrameReader = session.read();
+        // String parquetPath = args[0];
+
+        // Load data from a Parquet file
+        // Replace "path/to/your/file.parquet" with the actual file path
+        Dataset<Row> df = session.read().parquet("src/main/java/com/javadataframe/fileStore/data.parquet");
+
+        // Show the DataFrame contents
+        System.out.println("DataFrame content:");
+        df.show();
+
+        // Stop the Spark session
+        session.stop();
     }
 }
